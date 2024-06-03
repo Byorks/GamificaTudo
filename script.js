4b035f0e0d8a98eb60d4bd968b1cef86fc6b885b
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-06-03 10:21:45 -0300

    feat: API integration

--- script.js
+++ script.js
@@ -69,3 +69,39 @@ const enviarEmail = (event) => {
     event.preventDefault();
 }
 
+const solicitarOrcamento = (event) => {
+    let fieldName = document.getElementById("field-name").value;
+    let fieldEmail = document.getElementById("field-email").value;
+    let fieldDesc = document.getElementById("field-description").value;
+
+    let dataform = {
+        name: fieldName,
+        email: fieldEmail,
+        descricao: fieldDesc
+    }
+
+    console.log(dataForm)
+
+    fetch( "http://localhost:3000/solicitacao", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataform)
+    }) 
+
+    .then( resposta => {
+        console.log(resposta);
+
+        document.querySelector("#form form").reset();
+
+        alert("Sua solicitação a API foi um sucesso (●'◡'●)");
+    })
+
+    .catch( erro => {
+        console.log(erro);
+        alert("Algo deu erradoooo (╯▔皿▔)╯")
+    })
+
+    event.preventDefault();
+}
